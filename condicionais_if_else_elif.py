6685ef8b66825f83114064db8872fe4d91f2c778
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-07-09 20:56:24 -0300

    Término da aula sobre condicionais If, Else e Elif.

---
+++ condicionais_if_else_elif.py
@@ -0,0 +1,35 @@
+"""
+Estruturas condicionais
+If (Se), else, elif (else if)
+"""
+
+idade = 18
+
+"""
+Estrutura condicional if, else na linguagem C
+
+if (idade < 18){
+    printf("Menor de idade");
+}else if (idade = 18) {
+    printf("Tem 18 anos");
+}else{
+    printf("Maior de idade");
+}
+
+Estrutura condicional if, else na linguagem Java
+
+if (idade < 18){
+    System.out.println("Menor de idade");
+}else if (idade == 18) {
+    System.out.println("Tem 18 anos");
+}else{
+    System.out.println("Maior de idade");
+}
+"""
+
+if idade < 18:
+    print('Menor de idade')
+elif idade == 18:
+    print('Tem 18 anos')
+else:
+    print('Maior de idade')
