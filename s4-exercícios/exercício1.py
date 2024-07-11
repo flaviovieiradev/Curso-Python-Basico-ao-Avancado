a78f1bc0991ab3e7ecc5ec6dba8ae424bb521179
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-07-11 00:16:17 -0300

    Melhoria no código do exercício 1.

--- s4-exercícios/exercício1.py
+++ s4-exercícios/exercício1.py
@@ -2,8 +2,8 @@
 1. Faça um programa que receba dois números inteiros e mostre qual deles é o maior.
 """
 
-num1 = int(input('Digite o primeiro valor:'))
-num2 = int(input('Digite o segundo valor:'))
+num1: int = int(input('Digite o primeiro valor:'))
+num2: int = int(input('Digite o segundo valor:'))
 
 if num1 > num2:
     print(f"O primeiro número ({num1}) é maior que o segundo número ({num2}).")
