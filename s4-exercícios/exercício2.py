a47f0e5688b7bc4de77f249c1b2d9b175e0e956e
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-07-11 00:21:36 -0300

    Melhorias no código do Exercício 2.

--- s4-exercícios/exercício2.py
+++ s4-exercícios/exercício2.py
@@ -1,13 +1,13 @@
-import math
+from math import sqrt
 """
 2. Faça um programa que leia um número inteiro fornecido pelo usuário. Se esse número for positivo, calcule a 
 raiz quadrada do número e apresente-a.
 Se o número for negativo, mostre uma mensagem dizendo que o número é inválido.
 """
 
-numero = int(input("Digite um número inteiro: "))
+numero: int = int(input("Digite um número inteiro: "))
 
 if numero > 0:
-    print(f"A raiz quadrada do número", numero, "é", math.isqrt(numero))
+    print(f"A raiz quadrada do número {numero} é {sqrt(numero)}")
 else:
-    print(f"O número", numero, "é negativo.")
+    print(f"O número {numero} é inválido.")
