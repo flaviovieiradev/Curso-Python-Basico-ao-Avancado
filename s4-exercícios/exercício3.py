7e9480414ead18abf1cc7476dc97002788051007
flaviovieiradev <analyst.flaviovieira@gmail.com>
2024-07-11 00:26:02 -0300

    Melhorias no código do Exercício 3.

--- s4-exercícios/exercício3.py
+++ s4-exercícios/exercício3.py
@@ -2,9 +2,9 @@
 3. Faça um programa que recebe um número inteiro e informe se este número é par ou ímpar.
 """
 
-num = int(input("Digite um número: "))
+num: int = int(input("Digite um número: "))
 
-if (num % 2) == 0:
-    print(f"O número digitado é:", num, "Este número é par.")
+if num % 2 == 0:
+    print(f"O número digitado é: {num}. Este número é par.")
 else:
-    print(f"O número digitado é:", num, "Este número é ímpar.")
+    print(f"O número digitado é: {num}. Este número é ímpar.")
